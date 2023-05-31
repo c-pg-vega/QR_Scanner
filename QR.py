5055ec8806bf175c5ece10b31fda0ba7f8752f4f
Christian Vega <cpvega@umich.edu>
2023-05-31 19:29:10 -0400

    Basic Structure - No Working Code

---
+++ QR.py
@@ -0,0 +1,13 @@
+# This project is a personal project for the purpose of 
+# getting used to computer vision and image processing.
+
+# Importing the necessary libraries
+import cv2
+
+# main function
+def main():
+    return
+
+# Calling the main function
+if __name__ == "__main__":
+    main()
